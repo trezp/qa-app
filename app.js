e2ee830284708b6d42ea39d0fa6aafe07157ffe1
Treasure <treasure.rose@gmail.com>
2018-08-23 15:08:15 -0700

    helper functions

--- app.js
+++ app.js
@@ -7,37 +7,28 @@ const app = express();
 
 app.use(bodyParser.json({ type: 'application/json' }));
 
-
+// GET all questions
 app.get('/questions', (req, res) => res.json(data.questions));
 
+// GET specific question
 app.get('/questions/:qID', (req, res) => {
-  data.questions.forEach((question) =>{
-    if (req.params.qID == question.question_id) {
-      res.json(question)
-    } 
-  });
+  const question = getQuestion(req.params.qID);
+  res.json(question);
 }); 
 
+// GET all answers to specific question
 app.get('/questions/:qID/answers', (req, res) => {
-  data.questions.forEach((question) =>{
-    if (req.params.qID == question.question_id) {
-      res.json(question.answers)
-    } 
-  });
+  const answers = getQuestion(req.params.qID).answers
+  res.json(answers);
 }); 
 
+// GET specific answer to specific question
 app.get('/questions/:qID/answers/:aID', (req, res) => {
-  data.questions.forEach((question) =>{
-    if (req.params.qID == question.question_id) {
-      question.answers.forEach( (answer) => {
-        if(req.params.aID == answer.answer_id){
-          res.json(answer)
-        }
-      });
-    } 
-  });
+  const answer = getAnswer(req.params.qID, req.params.aID);
+  res.json(answer);
 }); 
 
+// POST a new question
 app.post('/questions', (req, res) => {
   
   data.questions.push({
@@ -50,12 +41,12 @@ app.post('/questions', (req, res) => {
   res.json(data);
 });
 
+// POST a new answer
 app.post('/questions/:qID', ( req, res ) => {
-  const questionID = req.params.qID;
-  console.log(data.questions.length)
+  const question = getQuestion(req.params.qID);
 
-  data.questions[questionID].answers.push({
-    "answer_id": `a${data.questions[questionID].answers.length}`, 
+  question.answers.push({
+    "answer_id": `a${question.answers.length}`, 
     "answer": req.body.answer,
     "votes": 0
   });
@@ -64,4 +55,32 @@ app.post('/questions/:qID', ( req, res ) => {
   res.json(data);
 });
 
+// DELETE questions (for testing only)
+app.get('/remove', (req, res) => {
+  data.questions = [];
+  fs.writeFile('data.json', JSON.stringify(data, null, 2));
+  res.json(data);
+}); 
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+
+////    HELPER FUNCTIONS  ////
+function getQuestion(id){
+  return data.questions.find(question => {
+    return question["question_id"] == id
+  });
+}
+
+function getAnswer( qID, aID){
+  return getQuestion(qID).answers.find( answer => {
+    return answer["answer_id"] == aID;
+  });
+}
+
+//TO DO 
+// error handling
+// delete a question
+// delete an answer
+// edit a question
+// edit an answer
