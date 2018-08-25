96cb945b28487cd0ea255fedb1d59b75cc13a53c
Treasure <treasure.rose@gmail.com>
2018-08-25 14:21:21 -0700

    Vote up and vote down functionality

--- app.js
+++ app.js
@@ -10,36 +10,39 @@ app.use(bodyParser.json({ type: 'application/json' }));
 // GET all questions
 app.get('/questions', (req, res) => res.json(data.questions));
 
+// POST Create a new question
+app.post('/questions', (req, res, err) => {
+  
+  if(req.body.question){
+    data.questions.push({
+      "question_id": data.questions.length, 
+      "question": req.body.question,
+      "answers": []
+    });
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), ()=>{
+      res.json(data);
+    });
+
+  } else {
+    res.status(400).json({error: "Oops! You must ask a question!"});
+  }
+  
+});
+
 // GET specific question
 app.get('/questions/:qID', (req, res) => {
   const question = getQuestion(req.params.qID);
-  res.json(question);
-}); 
-
-// GET all answers to specific question
-app.get('/questions/:qID/answers', (req, res) => {
-  const answers = getQuestion(req.params.qID).answers
-  res.json(answers);
-}); 
-
-// GET specific answer to specific question
-app.get('/questions/:qID/answers/:aID', (req, res) => {
-  const answer = getAnswer(req.params.qID, req.params.aID);
-  res.json(answer);
-}); 
-
-// POST a new question
-app.post('/questions', (req, res) => {
   
-  data.questions.push({
-    "question_id": data.questions.length, 
-    "question": req.body.question,
-    "answers": []
-  });
-
-  fs.writeFile('data.json', JSON.stringify(data, null, 2));
-  res.json(data);
-});
+  if (question){
+    res.json(question);
+  } else {
+    res.status(404).json({
+      error: "Question not found"
+    });
+  }
+  
+}); 
 
 // POST a new answer
 app.post('/questions/:qID', ( req, res ) => {
@@ -51,15 +54,45 @@ app.post('/questions/:qID', ( req, res ) => {
     "votes": 0
   });
 
-  fs.writeFile('data.json', JSON.stringify(data, null, 2));
-  res.json(data);
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), ()=>{
+    res.json(data);
+  });
+});
+
+// Vote up an answer up
+app.post('/questions/:qID/answers/:aID/vote-up', (req, res) => {
+  const answer = getAnswer(req.params.qID, req.params.aID);
+
+  if (answer){
+    answer.votes += 1
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), ()=>{
+      res.json(data);
+    });
+  } else {
+    res.status(404).json({error: "Answer not found"});
+  }
+});
+
+// Vote an answer down 
+app.post('/questions/:qID/answers/:aID/vote-down', (req, res) => {
+  const answer = getAnswer(req.params.qID, req.params.aID);
+  
+  if (answer){
+    answer.votes -= 1
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), ()=>{
+      res.json(data);
+    });
+  } else {
+    res.status(404).json({error: "Answer not found"});
+  }
 });
 
 // DELETE questions (for testing only)
 app.get('/remove', (req, res) => {
   data.questions = [];
-  fs.writeFile('data.json', JSON.stringify(data, null, 2));
-  res.json(data);
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), ()=>{
+    res.json(data);
+  });
 }); 
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
@@ -79,7 +112,8 @@ function getAnswer( qID, aID){
 }
 
 //TO DO 
-// error handling
+// Error handling for routes that don't exist 
+// If voting occurs to quickly server doesn't respond
 // delete a question
 // delete an answer
 // edit a question
